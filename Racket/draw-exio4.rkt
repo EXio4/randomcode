689187978d44cae7af7c63a23d98068655d7af7d
Esteban I. Ruiz Moreno <exio4.com@gmail.com>
2015-12-28 12:40:30 -0300

    add إبراهيم to draw-exio4.rkt update draw-exio4.rkt to use doubles

--- Racket/draw-exio4.rkt
+++ Racket/draw-exio4.rkt
@@ -4,55 +4,53 @@
 (require racket/stream)
 (require racket/dict)
 
-(define char '((#\e . ((#t #t #t)
-                       (#t #f #f)
-                       (#t #t #t)
-                       (#t #f #f)
-                       (#t #t #t)))
+(define char '((#\e . ((1.0 1.0 1.0)
+                       (1.0 0.0 0.0)
+                       (1.0 1.0 1.0)
+                       (1.0 0.0 0.0)
+                       (1.0 1.0 1.0)))
                
-               (#\x . ((#f #f #f)
-                       (#t #f #t)
-                       (#t #t #f)
-                       (#f #t #t)
-                       (#t #f #t)))
+               (#\x . ((0.0 0.0 0.0)
+                       (1.0 0.0 1.0)
+                       (1.0 1.0 0.0)
+                       (0.0 1.0 1.0)
+                       (1.0 0.0 1.0)))
                
-               (#\i . ((#f #f #f)
-                       (#t #t #t)
-                       (#f #t #f)
-                       (#f #t #f)
-                       (#t #t #t)))
+               (#\i . ((0.0 0.0 0.0)
+                       (1.0 1.0 1.0)
+                       (0.0 1.0 0.0)
+                       (0.0 1.0 0.0)
+                       (1.0 1.0 1.0)))
                
-               (#\o . ((#f #f #f)
-                       (#t #t #t)
-                       (#t #f #t)
-                       (#t #f #t)
-                       (#t #t #t)))
-               (#\4 . ((#t #f #t)
-                       (#t #f #t)
-                       (#t #t #t)
-                       (#f #f #t)
-                       (#f #f #t)
+               (#\o . ((0.0 0.0 0.0)
+                       (1.0 1.0 1.0)
+                       (1.0 0.0 1.0)
+                       (1.0 0.0 1.0)
+                       (1.0 1.0 1.0)))
+
+               (#\4 . ((1.0 0.0 1.0)
+                       (1.0 0.0 1.0)
+                       (1.0 1.0 1.0)
+                       (0.0 0.0 1.0)
+                       (0.0 0.0 1.0)
                        ))))
                   
 (define (get-char x)
   (dict-ref char x))
 
+(define (fold+map c z xs f)
+  (foldl (lambda (e es) (c es (f e))) z xs))
+
 (define (draw x c)
   (define bk (blank (pict-width x)))
-  (define (go-line xs)
-    (cond [(null? xs) bk]
-          [(car xs)   (hb-append x  (go-line (cdr xs)))]
-          [else       (hb-append bk (go-line (cdr xs)))]))
-  (define (go xs)
-    (cond [(null? xs) bk]
-          [else       (vl-append (go-line (car xs)) (go (cdr xs)))]))
-  (go (get-char c)))
+  (fold+map vl-append bk c
+            (lambda (ys) (fold+map hb-append bk ys
+                                   (lambda (e) (cellophane x e))))))
   
-
 
 (define (str-i s) (lambda (rect)
                 (foldr (lambda (curr next)
-                          (hb-append (draw rect (char-downcase curr)) next)) (blank (pict-width rect)) (string->list s))))
+                          (hb-append (draw rect (get-char (char-downcase curr))) next)) (blank (pict-width rect)) (string->list s))))
 
 
 (define (fractal i b func)
@@ -60,12 +58,29 @@
                    (func (bitmap (pict->bitmap v)))) b (in-range 0 i)))
 
 
-(define (rend file x) (begin (send (pict->bitmap x) save-file file 'png)
-                              x))
+(define (rend s file x) (begin (send (pict->bitmap x) save-file file 'png)
+                             (cond [(eq? s 'yes) x]
+                                   [else         (blank)])))
 
-(define s (str-i "exio4"))
-(rend "fractal-exio4.png" (fractal 2 (filled-rectangle 17 15) s))
-(rend "exio4-rect.png"    (s (filled-rectangle 16 15)))
-(rend "exio4-rrect.png"   (s (filled-rectangle 48 47)))
-(rend "exio4-circle.png"  (s (pin-over (colorize (disk 32) "red") 2 2 (disk 28))))
-(rend "exio4-rects.png"   (s (rectangle 32 32)))
+(define ibr
+       '((0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0)
+         (0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0)
+         (0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 1 0)
+         (0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 1 0)
+         (0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 1 0)
+         (0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 1 0)
+         (0 0 1 0 0 0 0 1 0 0 0 1 0 1 1 0 0 0 1 0 0 0 0 0 1 0 0 1 0 0 1 0)
+         (0 1 0 1 0 0 0 1 0 0 0 1 0 1 0 1 0 0 1 0 0 0 0 0 1 0 0 1 0 0 0 0)
+         (1 1 1 1 1 1 1 0 1 1 1 1 1 0 0 1 0 0 1 0 0 0 0 0 1 1 1 1 0 0 1 1)
+         (1 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 1 0)
+         (1 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 1 0 0 1 1)
+         (0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0)
+        ))
+
+(draw (filled-rectangle 9 9) ibr)
+;(define s (str-i "exio4"))
+;(rend 'no  "fractal-exio4.png" (fractal 2 (filled-rectangle 17 15) s))
+;(rend 'yes "exio4-rect.png"    (s (filled-rectangle 16 15)))
+;(rend 'yes "exio4-rrect.png"   (s (filled-rectangle 48 47)))
+;(rend 'yes "exio4-circle.png"  (s (pin-over (colorize (disk 32) "red") 2 2 (disk 28))))
+;(rend 'yes "exio4-rects.png"   (s (rectangle 32 32)))
